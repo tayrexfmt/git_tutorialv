b7775e0251c58b9080d94bb74aff8b784f12982e
tayrexfmt <tayrexfmt@gmail.com>
2021-08-13 21:12:23 +0100

    used function

--- Abdul-rafik.py
+++ Abdul-rafik.py
@@ -1,2 +1,5 @@
-introduction = "Hello World!, this is Abdul Rafik Al-hassan with HNGi7 ID HNG-05318 and email abdulrafikalhassan@yahoo.com using Python for Stage 2 task"
-print(introduction)
+def open():
+    introduction = "Hello World!, this is Abdul Rafik Al-hassan with HNGi7 ID HNG-05318 and email abdulrafikalhassan@yahoo.com using Python for Stage 2 task"
+    print(introduction)
+
+open()
